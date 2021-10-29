ef240913b1d29ae5161521e8a7d2a06bc00154d0
GabrielaNares <93278439+GabrielaNares@users.noreply.github.com>
2021-10-29 10:44:18 -0500

    Update librerias_kernel.py

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -1,4 +1,5 @@
 import numpy
+import cv2
  
 #Funcion para el Kernel del sombrero 
 #Parametros iniciales valor de sigma y tamaño de matriz (k)
@@ -24,3 +25,14 @@ def Laplacian(sigma, k):
         for j in range(k):
             matrix[i][j]=((i**2+j**2-2*sigma**2)/sigma**4)*math.exp(-1*(i**2+j**2)/2*sigma**2)(i-(k-1)/2,j-(k-1)/2,sigma)
     return matrix
+   
+def sepia():
+  img = cv2.imread('Sample.png')
+  original = img.copy()
+  img = cv2.transform(img, numpy.matrix([[0.272, 0.534, 0.131],
+                                      [0.349, 0.686, 0.168],
+                                      [0.393, 0.769, 0.189]])) # Imagen*matriz sepia 
+  img[numpy.where(img > 255)] = 255 # normalizando valores mayores que 255 
+  cv2.waitKey(0)
+  cv2.destroyAllWindows
+  return img
