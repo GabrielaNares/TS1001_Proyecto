9fa9e463cd8db33e5ad4f43ad671d8bb423383ac
Aderjarn <55771964+Aderjarn@users.noreply.github.com>
2021-10-29 10:29:19 -0500

    Update librerias_kernel.py

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -17,3 +17,10 @@ def gauss(sigma,k):
         for y in range (0,k):
             g[x][y]=1/(2*numpy.pi*sigma**2)*numpy.exp(-(x**2+y**2)/(2*sigma**2))
     return g
+
+   def Laplacian(sigma, k):
+    matix=[[0] * k for i in range(k)]
+    for i in range(k):
+        for j in range(k):
+            matrix[i][j]=((x**2+y**2-2*sigma**2)/sigma**4)*math.exp(-1*(x**2+y**2)/2*sigma**2)(i-(k-1)/2,j-(k-1)/2,sigma)
+    return matrix
