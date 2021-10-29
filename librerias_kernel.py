e4c2fcf8cdacd8a5658c518d481b309c95f9ef2a
Jazzareth <59776647+Jazzareth@users.noreply.github.com>
2021-10-29 10:40:45 -0500

    librerias_kernel.py
    se agrego comentario al código

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -1,12 +1,15 @@
 import numpy
  
+#Funcion para el Kernel del sombrero 
+#Parametros iniciales valor de sigma y tamaño de matriz (k)
 def kernel_sombrero(sigma,k):
-    matrix=numpy.zeros((k,k));
-    for x in range (0,k):
-        for y in range (0,k):
+    matrix=numpy.zeros((k,k)); #Crear matriz de K x K con valor 0
+    for x in range (0,k): # For para recorer todos los valores en x 
+        for y in range (0,k): #For para recorer todos los valores en y
+      #Formula para el filtro del sombrero en 2D
             matrix[x][y]=1/(numpy.pi*sigma**4)*(1-1/2*(x**2+y**2)/sigma**2)
             matrix[x][y]= matrix[x][y] * numpy.exp(-(x**2+y**2)/(2*sigma**2))
-    return matrix
+    return matrix #Regresamos la matriz con el filtro 
 
 def gauss(sigma,k):
     g=numpy.zeros((k,k));
