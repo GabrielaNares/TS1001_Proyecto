cbaa16336a7066ac9cc2592b187abdeb1bb28c4f
Jazzareth <59776647+Jazzareth@users.noreply.github.com>
2021-10-29 10:23:34 -0500

    Kernel_Guass
    Se agrego la función para obtener el Kernel de Gauss

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -10,3 +10,10 @@ def kernel_sombrero(sigma,k):
             matrix[x][y]=1/(numpy.pi*sigma**4)*(1-1/2*(x**2+y**2)/sigma**2)
             matrix[x][y]= matrix[x][y] * numpy.exp(-(x**2+y**2)/(2*sigma**2))
     return matrix
+
+def gauss(sigma,k):
+    g=numpy.zeros((k,k));
+    for x in range (0,k):
+        for y in range (0,k):
+            g[x][y]=1/(2*numpy.pi*sigma**2)*numpy.exp(-(x**2+y**2)/(2*sigma**2))
+    return g
