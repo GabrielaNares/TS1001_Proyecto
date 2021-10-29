2509ab2ecb36cf2175b6fc39f2c22695e6385713
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2021-10-29 10:19:59 -0500

    Update librerias_kernel.py

--- librerias_kernel.py
+++ librerias_kernel.py
@@ -1,1 +1,12 @@
-
+import matplotlib.pyplot as plt
+import numpy
+from PIL import Image
+from scipy import ndimage
+ 
+def kernel_sombrero(sigma,k):
+    matrix=numpy.zeros((k,k));
+    for x in range (0,k):
+        for y in range (0,k):
+            matrix[x][y]=1/(numpy.pi*sigma**4)*(1-1/2*(x**2+y**2)/sigma**2)
+            matrix[x][y]= matrix[x][y] * numpy.exp(-(x**2+y**2)/(2*sigma**2))
+    return matrix
